361f8db90bdda7bd57cb16e6f578183153d0d62e
aniltimalsina <timalsinaanil96@gmail.com>
2023-09-20 23:36:00 -0400

    add cloudy component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Sunny from './sunny.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Sunny from "./sunny.jsx";
+import Cloudy from "./cloudy.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Sunny />
-  </React.StrictMode>,
-)
+    <Cloudy />
+  </React.StrictMode>
+);
